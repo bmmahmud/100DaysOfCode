ef7c567ce76b08476440e384b2580f169f34180e
B.M.ASHIK MAHMUD <bmmahmud@gmail.com>
2018-09-24 22:33:35 +0600

    MVC

---
+++ project/ClientRejistration/Controllers/ClientController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientRejistration.Models;
+
+namespace ClientRejistration.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult AddorEdit(int id =0)
+        {
+            Client ClientModel = new Client();
+            return View(ClientModel);
+        }
+    }
+}
