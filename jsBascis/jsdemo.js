8561810e0c2a8fa75454bac6ce47326e7284b3af
B.M.ASHIK MAHMUD <bmmahmud@gmail.com>
2018-12-31 15:34:11 +0600

    JavaScript Basic DOM

---
+++ jsBascis/jsdemo.js
@@ -0,0 +1,33 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function alartF(name, age) {
+  alert("Hello " + name + age);
+}
+function person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+// var jhon = new person("Johon", 25);
+// var any = new person("Any", 27);
+// document.write(jhon.age + any.name);
+//Array>
+// var course = new Array("Html", "CSS", "JS");
+// var x = course[1];
+// document.write(course.length);
+// document.write(Math.PI);
+// var n = prompt("Enter a number", "");
+// var answer = Math.sqrt(n);
+// alert("The square root of " + n + " is " + answer);
+
+//Time
+function printTime() {
+  var d = new Date();
+  var hours = d.getHours();
+  var min = d.getMinutes();
+  var date = d.getDate();
+  var sec = d.getSeconds();
+  document.body.innerHTML = hours + ": " + min + ": " + sec;
+}
+setInterval(printTime, 1000);
