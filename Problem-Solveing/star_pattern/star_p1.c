e9fb4e75aa3c4fab0c0e7c65f9b34b5873c9fb40
B.M.ASHIK MAHMUD <bmmahmud@gmail.com>
2019-05-04 23:23:39 +0600

    star practise

---
+++ Problem-Solveing/star_pattern/star_p1.c
@@ -0,0 +1,22 @@
+/*
+           *
+          ***
+         *****
+        *******
+*/
+#include<stdio.h>
+void main(){
+int row,space,colm,n;
+printf("Number of Rows: ");scanf("%d",&n);
+for(row=1;row<=n;row++){
+     for(space=1;space<=n-row;space++)
+       printf(" ");
+       for(colm=1;colm<=2*row-1;colm++)
+            //printf("*");
+           //  printf("%d",colm);
+           //printf("%c",colm+64);
+
+       printf("\n");
+}
+
+}
