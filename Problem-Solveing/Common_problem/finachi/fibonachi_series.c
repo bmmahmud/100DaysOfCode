f5247f9583f7e0a6a9a061d14eedc8f244437631
B.M.ASHIK MAHMUD <bmmahmud@gmail.com>
2019-06-24 17:34:30 +0600

    fibonachi

---
+++ Problem-Solveing/Common_problem/finachi/fibonachi_series.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int first=0,second=1,fibo,n,count=0;
+printf("Enter Range: ");
+scanf("%d",&n);
+
+//check range
+while(count<n){
+    if(count<=1){
+        fibo=count;
+    }else{
+        fibo=first+second;
+        first=second;
+        second=fibo;
+    }
+    printf("%d ",fibo);
+    count++;
+}
+}
