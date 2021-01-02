3b00a8166fa5fdd7f9d11b730efd3f1297b041f4
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-01-02 23:50:38 +0600

    tuple completed

--- Problem-Solveing/Hackerrank_python/Tuples.py
+++ Problem-Solveing/Hackerrank_python/Tuples.py
@@ -1,4 +1,12 @@
-tup = ('aa','bb','ccc')
-print(tup)
-del tup
-print('after del',tup)
+#### Tuples
+# tup = ('aa','bb','ccc')
+# print(tup)
+# del tup
+# print('after del',tup)
+n = int(input())
+t = tuple(map(int,input().split()))
+print(hash(t))
+
+# New Thhings : map, tuple, hash - function 
+# 
+#
