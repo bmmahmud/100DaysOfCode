1977e5cf562bd1e90056500514090081e3c1c572
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-01-05 23:43:48 +0600

    list comprehension completed

--- Problem-Solveing/Hackerrank_python/List Comprehensions.py
+++ Problem-Solveing/Hackerrank_python/List Comprehensions.py
@@ -10,11 +10,26 @@
 # print("Using LC:",ListCom)
 
 ############################# using Condition
-movies = ["ashik","abir","lamia","afrin","sobuj","tanvir"]
-gmovies = []
-for title in movies:
-    if title.startswith("a"):
-        gmovies.append(title)
-print("Nomal G:",gmovies)
+# movies = ["ashik","abir","lamia","afrin","sobuj","tanvir"]
+# gmovies = []
+# for title in movies:
+#     if title.startswith("a"):
+#         gmovies.append(title)
+# print("Nomal G:",gmovies)
 
-# Using List Com  
+# # Using List Com 
+# names =  [title for title in movies if title.startswith("a")]
+# print("Condition LC:",names)
+
+############## More Complex
+# nameWithAge = [("ashik",25),("abir",19),("lamia",15),("afrin",16)]
+
+# names = [ name for (name,age) in nameWithAge if age >18]
+# print("Names: ",names)4
+# HackerRank Solution
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+s = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k !=n]
+print(s)
