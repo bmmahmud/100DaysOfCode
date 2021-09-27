b5b67efa82b641064c9286eef75d23235331c5d9
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-09-27 22:08:56 +0600

    string finish

--- Python/Python _Crash_course/Chapter_1/string.py
+++ Python/Python _Crash_course/Chapter_1/string.py
@@ -1,2 +1,22 @@
-name = "ada.loveelace"
+name = "ada lovelace"
 print(name.title())
+print(name.upper())
+print(name.lower())
+# concatination
+first_name = "ashik"
+last_name = "mahmud"
+print("Hello,"+first_name.title()+" "+last_name.title()+"!"+"\nMobile:121 \t (Bangladesh)")
+# reduce white space
+song = "Alpha . "
+print(song.rstrip())
+
+# Exercise
+name = "Eric"
+print("Hello "+name+" would you like to learn some python today?") # 2.3
+print("Hello "+name.upper()+" would you like to learn some python today?")
+print("Hello "+name.lower()+" would you like to learn some python today?")
+("Hello "+name.title()+" would you like to learn some python today?")
+famous_person = "\t Albert Einstein \n"
+print(famous_person+""" once said, "A person who never made a mistake never tried anything new." """)
+print(famous_person.rstrip()+""" once said, "A person who never made a mistake never tried anything new." """)
+print(famous_person.strip()+""" once said, "A person who never made a mistake never tried anything new." """)
