c7158efa4d51bd5b5f9fa4d9bacacb81909a5646
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-10-19 20:05:57 +0600

    list:append
    python crush course: started list up to append

---
+++ Python/Python _Crash_course/Basic/List.py
@@ -0,0 +1,14 @@
+# item = ["apple","banana","grave",'mango']
+# # print(item[-2].upper())
+
+# #assignment
+# print('my favourite frout is '+item[0].title())
+
+#Adding item in a list
+item = ['honda','yamaha','suzuki']
+print(item)
+item[0] = 'ducati'
+print("added item:"+str(item))
+#remove
+item.append('toyota')
+print("new item at end:"+str(item))
---
+++ Python/Python _Crash_course/Basic/number.py
@@ -0,0 +1,20 @@
+# 2 power 3 = 2*2*2 = 8
+# a = 2 ** 3
+# print(a)
+# # Order
+# b = (2 + 3) * 4
+# print(b)
+# print(0.2+0.1)
+
+# age= 23
+# # message = "Happy " + age + "rd Birthda!"
+# message = "Happy " + str(age) + "rd Birthda!"
+# print(message)
+
+# Assignment - Number 1
+# print('{0} \n{1} \n{2} \n{3}'.format((3+5.0),(10.0-2),(4.0*2),(16.0/2)))
+# # Assignment - Number 2
+# fvtNumber = 2
+# print("My Favourite Number is "+str(fvtNumber)+"!")
+
+import this
