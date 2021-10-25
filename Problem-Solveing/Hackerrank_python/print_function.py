d03367a8d955a8f7101f2b9ca5fe429c34fd7a11
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-10-26 00:59:54 +0600

    List: List Assignment

---
+++ Problem-Solveing/Hackerrank_python/print_function.py
@@ -0,0 +1,5 @@
+# Hacker Rank - Print Function
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n):
+        print(i)
--- Python/Python _Crash_course/Basic/List.py
+++ Python/Python _Crash_course/Basic/List.py
@@ -1,14 +1,58 @@
+#Notes - alter item, set new item at last by append, 
+# and insert new item
+
+
 # item = ["apple","banana","grave",'mango']
 # # print(item[-2].upper())
 
 # #assignment
 # print('my favourite frout is '+item[0].title())
 
-#Adding item in a list
-item = ['honda','yamaha','suzuki']
-print(item)
-item[0] = 'ducati'
-print("added item:"+str(item))
-#remove
-item.append('toyota')
-print("new item at end:"+str(item))
+## Adding item in a list
+# item = ['honda','yamaha','suzuki']
+# print(item)
+# item[0] = 'ducati' # alter item
+# print("added item:"+str(item))
+# #remove
+# item.append('toyota')
+# print("new item at end:"+str(item))
+
+# items = []
+# items.append('apple')
+# items.append('orange')
+# items.append('mango')
+# items.append('watermalon')
+# print(items)
+# #insert
+# items.insert(3,'New Apple')
+# print(items)
+## removeig items
+# del items[3]
+# print(items)
+# popped_items = items.pop()
+# print(items)
+# print(popped_items)
+# popped_items = items.pop()
+# print(items)
+# print(popped_items)
+# popped_items = items.pop()
+# print(items)
+# print(popped_items)
+
+# items.remove('New Apple')
+# print(items)
+
+## Assisnment 3.4
+print("Invite 3 people for Dinner.\n Enter name:")
+names = []
+names.append('Ashik')
+names.append('Abir')
+names.append('Lamia')
+invitation = 'You are Invited in my Dinner.'
+print(names[0],",\n",invitation)
+print(names[1],",\n",invitation)
+print(names[2],",\n",invitation)
+print(names[1],',\n Is Unable to attend the party.')
+names[1]='Emma'
+print(names[1],",\n",invitation)
+print('Lists of all Members:',names)
