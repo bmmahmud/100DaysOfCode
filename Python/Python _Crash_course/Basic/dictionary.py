10cf9f6768002601f51fcc8df9ac0801be40656e
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-12-08 20:53:52 +0600

    python Dictionary: chapter 2

--- Python/Python _Crash_course/Basic/dictionary.py
+++ Python/Python _Crash_course/Basic/dictionary.py
@@ -26,6 +26,35 @@ alien_0['x_position'] = alien_0['x_position'] + x_increment
 # print("New x-position: " + str(alien_0['x_position']))
 
 ## Remove item from Dic
-print(alien)
-del alien['points']
-print(alien)
+# print(alien)
+# del alien['points']
+# print(alien)
+
+# Assignment 6.1
+person = {'name':'Ashik','age':28,'location':'Mirpur','zipcode':1216}
+# for info in person:
+    # print('Name: '+str(person['name']).title()+'\n'
+    # 'Age: '+str(person['age'])+'\n'
+    # 'Location: '+str(person['location'].title())+'\n'
+    # 'Zipcode:'+str(person['zipcode'])+'.'
+    # )
+
+# Ass 6.2 with new loops
+birthDate = {'Abir':11,'Lamia':8,'Ashik':8,'Rasel':24,'Shila':16,'Mim':17}
+# for name,bdate in birthDate.items():
+    # print(name.title()+"'s Birthday in June "+str(bdate)) 
+
+## Only accress Key
+# for name in birthDate.keys():
+    # print(name)
+## sorted key
+# for name in sorted(birthDate.keys()):
+#     print(name+" Hi their.")  
+
+## print only values
+# for age in birthDate.values():
+#     print(age)
+## WithOut repeatation 
+for age in set(birthDate.values()):
+    print(age)
+
