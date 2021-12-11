45e25324f6c3b442c244d2fc727e8b0f0c5086a3
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2021-12-11 23:54:56 +0600

    Dictionary final assignment.

--- Python/Python _Crash_course/Basic/dictionary.py
+++ Python/Python _Crash_course/Basic/dictionary.py
@@ -109,11 +109,21 @@ users = {
     }
 }
 
-for username, user_info in users.items():
-    print("\nUsername: "+ username)
-    full_name = user_info['first'] + " " + user_info['last']
-    location = user_info['location']
+# for username, user_info in users.items():
+#     print("\nUsername: "+ username)
+#     full_name = user_info['first'] + " " + user_info['last']
+#     location = user_info['location']
 
-    print("\tFull Name: " + full_name.title())
-    print("\tLocation: " + location.title())
+#     print("\tFull Name: " + full_name.title())
+#     print("\tLocation: " + location.title())
+
+##### Assignment : 6-7
+people = [
+    person_1 = {'name':'Ashik','age':28,'location':'Mirpur','zipcode':1216},
+    person_2 = {'name':'Ashik','age':28,'location':'Mirpur','zipcode':1216},
+    person_3 = {'name':'Ashik','age':28,'location':'Mirpur','zipcode':1216}
+]
+for person in people:
+    
+
 
