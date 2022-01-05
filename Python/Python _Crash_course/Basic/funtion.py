5e3baa4533ee5dc55bcb17fc737e96d769c434a2
B.M.ASHIK MAHMUD <31992585+bmmahmud@users.noreply.github.com>
2022-01-06 00:27:06 +0600

    updateing

--- Python/Python _Crash_course/Basic/funtion.py
+++ Python/Python _Crash_course/Basic/funtion.py
@@ -190,15 +190,15 @@
 # pizza.make_pizza(12,'mushrooms','green peppers','extra cheese')
 
 # calling specific function from a module
-from pizza import make_pizza 
-make_pizza(16,'pepperoni')
-make_pizza(12,'mushrooms','green peppers','extra cheese')
+# from pizza import make_pizza 
+# make_pizza(16,'pepperoni')
+# make_pizza(12,'mushrooms','green peppers','extra cheese')
 
 ### Using as to Give a Function an Alias
 from pizza import make_pizza as mp
 
-mp(16,'pepperoni')
-mp(12,'mushrooms','green peppers','extra cheese')
+# mp(16,'pepperoni')
+# mp(12,'mushrooms','green peppers','extra cheese')
 
 ### Call all function
 # from pizza import * 
